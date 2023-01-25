df83f2e54a0ba85cee1d0972e260dbddf5f90379
Poorvaahuja <122693422+Poorvaahuja@users.noreply.github.com>
2023-01-25 20:04:30 +0530

    Submitting q8 of assignment2

---
+++ A2_q8.py
@@ -0,0 +1,12 @@
+n = int(input('Enter the the total no. of rows to be printed:'))
+def Pattern(n):
+    sp = n-1
+    st = 1
+    for i in range(1, n + 1):
+        for space in range(1, sp + 1):
+            print(end="\t");
+        for star in range(1, st + 1):
+            print('*', end="\t");
+        sp = sp-1
+        print("\n")
+Pattern(n)
